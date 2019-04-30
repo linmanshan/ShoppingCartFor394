4f09e0c54f4637483a9ff840d92ecb6962bab1f1
林漫山 <1284568390@qq.com>
2019-04-29 19:14:56 -0500

    implementCart

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import {
+  ButtonToolbar,
+  Button,
+  DropdownButton,
+  Dropdown
+} from "react-bootstrap";
 
-
-
-class ShoppingCart extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            
-        }
-    }
+class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const names = Object.keys(this.props.shoppedItem);
+    console.log(names);
+    return (
+      <ButtonToolbar>
+        <Dropdown>
+          <Dropdown.Toggle>Pow! Zoom!</Dropdown.Toggle>
+          <Dropdown.Menu>
+            {names.map(product => {
+              return (
+                <Dropdown.Item key={product}>
+                  <div>Name:{product}</div>
+                  <div>Price:{this.props.shoppedItem[product][0]}</div>
+                  <div>Quantity:{this.props.shoppedItem[product][1]}</div>
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      </ButtonToolbar>
+    );
+  }
 }
+
+export default ShoppingCart;
