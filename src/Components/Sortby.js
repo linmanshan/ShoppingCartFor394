fe0bd4d5da1d87cc888ee4fef00f83bb76884831
林漫山 <1284568390@qq.com>
2019-05-01 09:09:38 -0500

    addSortby

---
+++ src/Components/Sortby.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import {
+  ButtonToolbar,
+  Button,
+  DropdownButton,
+  Dropdown
+} from "react-bootstrap";
+
+class Sortby extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <DropdownButton title="Order By">
+        <Dropdown.Item onClick={() => this.props.LowToHigh()}>
+          Lowest to Highest
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => this.props.HighToLow()}>
+          Highest to Lowest
+        </Dropdown.Item>
+      </DropdownButton>
+    );
+  }
+}
+
+export default Sortby;
